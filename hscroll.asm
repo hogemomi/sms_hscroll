62fb5178b6bc852db8dbe57d5e9d0cf515fa947e
hogel <104550793+hogemomi@users.noreply.github.com>
2025-06-05 08:55:27 +0900

    hscroll.asm を更新

--- hscroll.asm
+++ hscroll.asm
@@ -261,6 +261,19 @@ drawcolumn_loop:
     sbc hl,bc
     ld (NextColSrc),hl
 
+; Check Vramadd of final
+    ld hl,(NextColVram)
+    ld bc,$05fe
+    add hl,bc
+    ld de,ScreenBottomVram
+    ld a,l
+    cp e
+    jp nz,mainloop
+
+    ld a,h
+    cp e
+    jp nz,mainloop
+
 ; Return first vram address
 ret_1st_vramadd
     ld hl,$3800
