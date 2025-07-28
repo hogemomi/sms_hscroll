5ee9368a910df523b0bb219eb7d9d96fb12f5480
hogel <104550793+hogemomi@users.noreply.github.com>
2025-07-28 20:03:59 +0900

    Slow Hscroll2.asm を更新

--- Slow Hscroll2.asm
+++ Slow Hscroll2.asm
@@ -181,13 +181,6 @@ mainloop:
     call wait_vblank
 
 ; -------------------
-; Map end check
-    ld a,(Scroll)
-    cp $00
-    jr z,screen_cnt
-    jp mainloop
-
-; -------------------
 ; Draw Column Timing check every 8px scroll
     ld a,(Scroll)
     and %00000111
@@ -216,6 +209,12 @@ mainloop:
     ld a,h
     cp $01
     call z,initialize_fixedpoint
+
+; -------------------
+; Map end check
+    ld a,(Scroll)
+    cp $00
+    jp nz,mainloop
 
 screen_cnt:
     ld a,(ScreenCount)
